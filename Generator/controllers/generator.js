7bbda8606ab31f3b36981e44727de2ca5359405d
bellotti florian <florian.bellotti@viacesi.fr>
2016-07-04 11:28:58 +0200

    generate products in commande

--- Generator/controllers/generator.js
+++ Generator/controllers/generator.js
@@ -7,6 +7,10 @@ function randomDate(start, end) {
     return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
 }
 
+function randomNumber(min, max) {
+    return Math.floor((Math.random() * max) + min);
+}
+
 // return the commande view  
 function getCommandeView(req, res){
     res.status(200).render('commande', {commandes : []});
@@ -44,23 +48,59 @@ function addCommande(req, res){
 
     req.on('end', function () {
         var jsonObj = JSON.parse(body);
+        commandeData.getAllCommande(function(err, commandeList) {
+            commandeLength = commandeList.length;
 
-        var dateList = [];
-        for (var i = 0; i < jsonObj.quantity; i++) {
-            var insert = [];
-            insert.push(randomDate(new Date(jsonObj.dateMin), new Date(jsonObj.dateMax)));
-            dateList.push(insert);
-        }
+            commandeData.getAllProduit(function(err, produitList) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    var dateList = [];
+                    var dateInsert = [];
+                    var commandeProduits = [];
+                    var commandeProduitInsert = [];
+                    var quantity = 0;
+                    var id = 0;
+                    var numberDifferentProducts = 0;
 
-        commandeData.addCommande(dateList, function(err) {
-            if (err) {
-                console.log(err);
-            } else {
-                res.status(200).send();
-            }
+                    // generate all commande
+                    for (var i = 0; i < jsonObj.quantity; i++) {
+                        // generate commande
+                        commandeLength += 1;
+                        dateInsert = [];
+                        dateInsert.push(commandeLength, randomDate(new Date(jsonObj.dateMin), new Date(jsonObj.dateMax)));
+                        dateList.push(dateInsert);
+
+                        // generate products
+                        numberDifferentProducts = randomNumber(1,4);
+                        for (var y = 0; y < numberDifferentProducts; y++) {
+                            commandeProduitInsert = [];
+                            quantity = randomNumber(1,15);
+                            id = randomNumber(1,produitList.length-1);
+                            commandeProduitInsert.push(commandeLength, quantity, produitList[id].id);
+                            commandeProduits.push(commandeProduitInsert);
+                        }
+                    }
+
+                    // insert in db
+                    console.log(dateList);
+                    commandeData.addCommande(dateList, function(err) {
+                        if (err) {
+                            console.log(err);
+                        } else {
+
+                            commandeData.addCommandeProduit(commandeProduits, function(err) {
+                                if (err) {
+                                    console.log(err);
+                                } else {
+                                    res.status(200).send();
+                                }
+                            });
+                        }
+                    });
+                }
+            });
         });
-        
-
     })
 }
 
@@ -68,7 +108,7 @@ function addCommande(req, res){
 router.get('/', getCommandeView);
 router.get('/list', getAllCommande);
 router.post('/add', addCommande);
-router.get('/truncate', truncateTable);
+//router.get('/truncate', truncateTable);
 
 
 // export router
--- Generator/models/db.js
+++ Generator/models/db.js
@@ -2,7 +2,7 @@ var mysql      = require('mysql');
 
 // define the connection
 var connection = mysql.createConnection({
-  host     : '192.168.10.164',
+  host     : '192.168.10.165',
   user     : 'root',
   password : 'root',
   database : 'administration'
@@ -16,24 +16,58 @@ exports.getAllCommande = function getAllCommande(callback) {
 	request = "SELECT * FROM commande;"
 
 	connection.query(request, function(err, rows, fields) {
-  		callback(err, rows);
-  	});
+  	callback(err, rows);
+  });
+};
+
+// select all in table produit
+exports.getAllProduit = function getAllProduit(callback) {
+  request = "SELECT * FROM produit;"
+
+  connection.query(request, function(err, rows, fields) {
+    callback(err, rows);
+  });
+};
+
+// select all in table produit
+exports.getAllCommandeProduit = function getAllProduitCommande(callback) {
+  request = "SELECT * FROM produit;"
+
+  connection.query(request, function(err, rows, fields) {
+    callback(err, rows);
+  });
 };
 
 // insert table commande
-exports.addCommande = function addCommande(dateList, callback) {
-	request = "INSERT INTO commande (date_enregistrement) VALUES ?";
+exports.addCommande = function addCommande(insert, callback) {
+	request = "INSERT INTO commande (id, date_enregistrement) VALUES ?";
 
-	connection.query(request, [dateList], function(err, rows, fields) {
-  		callback(err, rows);
-  	});
+	connection.query(request, [insert], function(err, rows, fields) {
+  	callback(err, rows);
+  });
 };
+
+// insert table commande_produit
+exports.addCommandeProduit = function addCommande(insert, callback) {
+  request = "INSERT INTO commande_produit (id_commande, quantite, id_produit) VALUES ?";
+
+  connection.query(request, [insert], function(err, rows, fields) {
+    callback(err, rows);
+  });
+};
+
 
 // truncate table commande
-exports.truncateCommande = function truncateCommande(callback) {
-	request = "TRUNCATE TABLE commande;";
+// exports.truncateCommande = function truncateCommande(callback) {
+// 	request1 = "SET FOREIGN_KEY_CHECKS = 0;";
+//   request2 = "TRUNCATE TABLE commande_produit, commande;";
+//   request3 = "SET FOREIGN_KEY_CHECKS = 1;";
 	
-	connection.query(request, function(err, rows, fields) {
-  		callback(err, rows);
-  	});
-};
+// 	connection.query(request1, function(err, rows, fields) {
+//   	 connection.query(request2, function(err, rows, fields) {
+//       connection.query(request3, function(err, rows, fields) {
+//         callback(err, rows);
+//       });
+//     });
+//   });
+// };
